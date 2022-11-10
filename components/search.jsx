5d11b422e86a76053d7c274134f27f46de3249a7
oliverjam <oliverjamesphillips@gmail.com>
2022-11-10 12:13:55 +0000

    Simplify Search form

--- components/search.jsx
+++ components/search.jsx
@@ -13,12 +13,18 @@ import { Icon } from "../components/icon.jsx";
 export function Search() {
   return (
     <>
-      <form action="https://duckduckgo.com" target="_blank" rel="noopener">
+      <form
+        class="Search"
+        action="https://duckduckgo.com"
+        target="_blank"
+        rel="noopener"
+        onsubmit="this.elements.k7.value = getComputedStyle(document.documentElement).backgroundColor"
+      >
         <input type="hidden" name="ko" value="-2" />
         <input type="hidden" name="k1" value="-1" />
         <input type="hidden" name="kz" value="-1" />
         <input type="hidden" name="km" value="m" />
-        <input type="hidden" name="k7" value="#fafef5" id="ddgbg" />
+        <input type="hidden" name="k7" value="#fafef5" />
         <input type="hidden" name="sites" value="oliverjam.es" />
         <div class="grid pile items-center">
           <input
@@ -28,29 +34,9 @@ export function Search() {
             placeholder="Search"
             class="SearchInput"
           />
-          <svg
-            id="search"
-            class="color-bright"
-            stroke="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="2"
-            width="24"
-            height="24"
-            style="margin-inline-start: var(--space-3)"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z"
-            />
-          </svg>
+          <Icon name="search" size="20" />
         </div>
       </form>
-      <script>{`
-      ddgbg.value = getComputedStyle(document.documentElement).backgroundColor
-    `}</script>
     </>
   );
 }
