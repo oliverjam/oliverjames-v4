253c56625719cb31baf654da7938a1ef2807507c
oliverjam <oliverjamesphillips@gmail.com>
2022-11-10 12:11:30 +0000

    SVG icons should be aria-hidden

--- components/icon.jsx
+++ components/icon.jsx
@@ -1,6 +1,6 @@
 export function Icon({ size = "20", name, ...rest }) {
   return (
-    <svg {...rest} width={size} height={size}>
+    <svg {...rest} width={size} height={size} aria-hidden="true">
       <use href={`/assets/sprite.svg#${name}`} />
     </svg>
   );
