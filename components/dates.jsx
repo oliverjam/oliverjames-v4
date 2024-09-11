58c094614875fd92c98e062696249fccb4399005
oliverjam <oliverjamesphillips@gmail.com>
2024-09-11 23:09:50 +0100

    Make datetimes valid on <time> elements

--- components/dates.jsx
+++ components/dates.jsx
@@ -3,8 +3,8 @@ import { readable } from "../lib/dates.js";
 export function ReadableDate({ children, ...overrides }) {
   return (
     <time
-      datetime={children.toString()}
-      title={children.toString()}
+      datetime={children.toISOString()}
+      title={children.toLocaleString("en-GB")}
       class="dt-published"
     >
       {readable(children, overrides)}
